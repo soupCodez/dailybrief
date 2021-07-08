5346c992726f83b9794083ae018923ee4359ae23
soupCodez <57576747+soupCodez@users.noreply.github.com>
2021-07-08 19:43:44 -0400

    minor: fixed fetch hopefully

--- fetch/index.js
+++ fetch/index.js
@@ -12,17 +12,22 @@ const argv = yargs(hideBin(process.argv)).argv;
 
 const axios = require('axios');
 
-const get = axios.get();
-
 const fs = require('fs');
 
-const result = await get(
-  `https://newsapi.org/v2/everything?q=tesla&from=2021-06-05&sortBy=publishedAt&apiKey=${argv.token}`
-);
+const NewsAPI = require('newsapi');
+const { fstat } = require('fs/promises');
+const newsapi = new NewsAPI(argv.token.toString());
 
-fs.writeFileSync('../news.json', result, (err) => {
-  if (err) {
-    console.error(err);
-  }
-});
+newsapi.v2
+  .topHeadlines({
+    language: 'en',
+  })
+  .then((response) => {
+    try {
+      fs.writeFileSync('news.json', JSON.stringify(response));
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
 /* eslint-enable import/no-unresolved */
